573fe9ed4dc9100cc606c1c35e0dd0c61a2dc635
Vishnu Pillai <vishnu.pillai@mountblue.io>
2018-09-06 16:43:43 +0530

    passwords are now stored in encrypted form

--- router/home.js
+++ router/home.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const authDB = require('../config/signIn.js');
 
 const home = express.Router();
@@ -15,6 +16,8 @@ home.post('/register', async (req, res) => {
   try {
     const user = await (req.body);
     const dbo = await authDB.authDB();
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
     await dbo.collection('user').insertOne(user);
     res.send(`User Registered: ${user.email}`);
   } catch (err) {
